0a89d0b68f36d61b9dc202e95ed8de856676ab33
MabinogiCode <103588171+MabinogiCode@users.noreply.github.com>
2025-09-19 23:51:53 +0200

    feat(theme): add DarkTheme and series palette; rendering: draw all LineSeries using theme palette (#14)
    Co-authored-by: Florent MENOT <florent.menot@setec.com>

--- demos/DemoApp.Net48/ViewModels/MainViewModel.cs
+++ demos/DemoApp.Net48/ViewModels/MainViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using FastCharts.Core;
 using FastCharts.Core.Primitives;
 using FastCharts.Core.Series;
+using FastCharts.Core.Themes.BuiltIn;
 
 namespace DemoApp.Net48.ViewModels
 {
@@ -19,7 +20,7 @@ namespace DemoApp.Net48.ViewModels
             var points = Enumerable.Range(0, 101)
                 .Select(i => new PointD(i, Math.Sin(i * Math.PI / 20.0)))
                 .ToArray();
-
+            
             Chart.AddSeries(new LineSeries(points));
             Chart.UpdateScales(800, 400); // nominal size; real renderer will update on arrange
         }
---
+++ src/FastCharts.Core/Themes/BuiltIn/DarkTheme.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+using FastCharts.Core.Abstractions;
+using FastCharts.Core.Primitives;
+
+namespace FastCharts.Core.Themes.BuiltIn;
+
+public sealed class DarkTheme : ITheme
+{
+    public ColorRgba AxisColor       => new(180, 180, 180);
+    public ColorRgba GridColor       => new(120, 120, 120, 40);
+    public ColorRgba LabelColor      => new(190, 190, 190);
+    public double    AxisThickness   => 1;
+    public double    GridThickness   => 1;
+    public double    TickLength      => 5;
+    public double    LabelTextSize   => 12;
+    public ColorRgba PrimarySeriesColor => new(51, 153, 255);
+
+    // pleasant, high-contrast palette for multiple series
+    public IReadOnlyList<ColorRgba> SeriesPalette => _palette;
+    private static readonly ColorRgba[] _palette =
+    {
+        new( 51, 153, 255), // blue
+        new(255, 128,  64), // orange
+        new( 60, 220, 130), // green
+        new(220,  70, 140), // pink
+        new(155, 120, 255), // purple
+        new(255, 200,  60), // yellow
+    };
+}
