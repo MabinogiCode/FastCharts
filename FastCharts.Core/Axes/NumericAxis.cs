5e0bc3ba49eecf602ba9c8ec14c62f55ae004eaf
Florent MENOT <florent.menot@setec.com>
2025-09-19 11:49:57 +0200

    feat(core): add minimal Axis/Scale/Ticker and Viewport scaffolding

---
+++ FastCharts.Core/Axes/NumericAxis.cs
@@ -0,0 +1,28 @@
+﻿using FastCharts.Core.Abstractions;
+using FastCharts.Core.Primitives;
+using FastCharts.Core.Scales;
+using FastCharts.Core.Ticks;
+
+namespace FastCharts.Core.Axes;
+
+public sealed class NumericAxis : IAxis<double>
+{
+    public NumericAxis()
+    {
+        Scale = new LinearScale(0, 1, 0, 1);
+        Ticker = new NumericTicker();
+        DataRange = new FRange(0, 1);
+        VisibleRange = new FRange(0, 1);
+    }
+
+    public IScale<double> Scale { get; private set; }
+    public ITicker<double> Ticker { get; }
+    public FRange DataRange { get; set; }
+    public FRange VisibleRange { get; set; }
+    public string? LabelFormat { get; set; } = "G";
+
+    public void UpdateScale(double pixelMin, double pixelMax)
+    {
+        Scale = new LinearScale(VisibleRange.Min, VisibleRange.Max, pixelMin, pixelMax);
+    }
+}
---
+++ FastCharts.Core/Scales/LinearScale.cs
@@ -0,0 +1,31 @@
+﻿using FastCharts.Core.Abstractions;
+
+namespace FastCharts.Core.Scales;
+
+public sealed class LinearScale : IScale<double>
+{
+    private readonly double _dataMin;
+    private readonly double _dataMax;
+    private readonly double _pixelMin;
+    private readonly double _pixelMax;
+
+    public LinearScale(double dataMin, double dataMax, double pixelMin, double pixelMax)
+    {
+        _dataMin = dataMin;
+        _dataMax = dataMax;
+        _pixelMin = pixelMin;
+        _pixelMax = pixelMax;
+    }
+
+    public double ToPixels(double value)
+    {
+        var t = (value - _dataMin) / (_dataMax - _dataMin);
+        return _pixelMin + t * (_pixelMax - _pixelMin);
+    }
+
+    public double FromPixels(double px)
+    {
+        var t = (px - _pixelMin) / (_pixelMax - _pixelMin);
+        return _dataMin + t * (_dataMax - _dataMin);
+    }
+}
