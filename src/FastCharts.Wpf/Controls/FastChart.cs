7dc8a8b6da354158a7bf56ae7114eb3265d12c72
MabinogiCode <MabinogiCode@users.noreply.github.com>
2025-09-19 14:27:11 +0200

    feat(wpf): add FastChart control with ChartModel DP; demo: bind model in .NET 8 app

---
+++ src/FastCharts.Wpf/Controls/FastChart.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using FastCharts.Core;
+
+namespace FastCharts.Wpf.Controls
+{
+
+    public class FastChart : Control
+    {
+        static FastChart()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(FastChart),
+                new FrameworkPropertyMetadata(typeof(FastChart)));
+        }
+
+        public ChartModel? Model
+        {
+            get => (ChartModel?)GetValue(ModelProperty);
+            set => SetValue(ModelProperty, value);
+        }
+
+        public static readonly DependencyProperty ModelProperty =
+            DependencyProperty.Register(
+                nameof(Model),
+                typeof(ChartModel),
+                typeof(FastChart),
+                new FrameworkPropertyMetadata(default(ChartModel), FrameworkPropertyMetadataOptions.AffectsRender));
+    }
+}
