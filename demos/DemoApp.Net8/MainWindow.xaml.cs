c8e4d9c25c88f5533d43d5fbc59dc17eafb065de
Florent MENOT <florent.menot@setec.com>
2025-09-19 14:27:11 +0200

    feat(wpf): add FastChart control with ChartModel DP; demo: bind model in .NET 8 app

--- demos/DemoApp.Net8/MainWindow.xaml.cs
+++ demos/DemoApp.Net8/MainWindow.xaml.cs
@@ -1,30 +1,14 @@
-
-using System;
-using System.Linq;
 using System.Windows;
-using System.Windows.Media;
-using DynamicData;
-using FastCharts.Wpf.Contracts;
+using DemoApp.Net8.ViewModels;
 
 namespace DemoApp
 {
     public partial class MainWindow : Window
     {
-        public System.Collections.ObjectModel.ReadOnlyObservableCollection<LineSeries> Series { get; }
-        private readonly SourceList<LineSeries> _source = new();
 
         public MainWindow()
         {
-            InitializeComponent();
-            _source.Connect().Bind(out var series).Subscribe();
-            Series = series;
-            int n=100_000; var rand=new Random();
-            var xs=Enumerable.Range(0,n).Select(i=>(double)i/100).ToArray();
-            var sine=xs.Select(Math.Sin).ToArray();
-            var noise=xs.Select(_=>rand.NextDouble()*2-1).ToArray();
-            _source.Add(new LineSeries(xs,sine,"Sine"){ Stroke=Brushes.SteelBlue, StrokeThickness=1.0, DrawLines=true, ShowPoints=false });
-            _source.Add(new LineSeries(xs,noise,"Noise"){ Stroke=Brushes.OrangeRed, StrokeThickness=1.0, DrawLines=true, ShowPoints=false });
-            DataContext=this;
+            DataContext = new MainViewModel();
         }
     }
 }
