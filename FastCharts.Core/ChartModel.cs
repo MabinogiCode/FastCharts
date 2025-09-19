a7a7e3e42a20f81989a75894953a25708f30313d
MabinogiCode <MabinogiCode@users.noreply.github.com>
2025-09-19 13:13:15 +0200

    feat(core): add ChartModel and LineSeries (data-only) with unit tests

---
+++ FastCharts.Core/ChartModel.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+
+using FastCharts.Core.Abstractions;
+using FastCharts.Core.Axes;
+using FastCharts.Core.Primitives;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace FastCharts.Core;
+
+public sealed class ChartModel : IChartModel
+{
+    public ChartModel()
+    {
+        XAxis = new NumericAxis();
+        YAxis = new NumericAxis();
+        Viewport = new Interactivity.Viewport(new FRange(0, 1), new FRange(0, 1));
+
+        Series = new ObservableCollection<object>();
+        Axes = new ReadOnlyCollection<object>(new object[] { XAxis, YAxis });
+    }
+
+    public NumericAxis XAxis { get; }
+    public NumericAxis YAxis { get; }
+    public IViewport Viewport { get; }
+    public ObservableCollection<object> Series { get; }
+    public IReadOnlyList<object> Axes { get; }
+
+    // IChartModel (kept loose for now)
+    IReadOnlyList<object> IChartModel.Axes => Axes;
+    IReadOnlyList<object> IChartModel.Series => Series;
+
+    public void AddSeries(object series)
+    {
+        Series.Add(series);
+        AutoFitDataRange();
+    }
+
+    public void ClearSeries()
+    {
+        Series.Clear();
+        XAxis.DataRange = new FRange(0, 1);
+        YAxis.DataRange = new FRange(0, 1);
+        Viewport.SetVisible(XAxis.DataRange, YAxis.DataRange);
+    }
+
+    public void UpdateScales(double widthPx, double heightPx)
+    {
+        XAxis.VisibleRange = Viewport.X;
+        YAxis.VisibleRange = Viewport.Y;
+        XAxis.UpdateScale(0, widthPx);
+        // Y pixels typically grow downward; invert here for convenience
+        YAxis.UpdateScale(heightPx, 0);
+    }
+
+    public void AutoFitDataRange()
+    {
+        var xs = new List<FRange>();
+        var ys = new List<FRange>();
+
+        foreach (var s in Series)
+        {
+            if (s is Series.LineSeries ls && !ls.IsEmpty)
+            {
+                xs.Add(ls.GetXRange());
+                ys.Add(ls.GetYRange());
+            }
+        }
+
+        if (xs.Count == 0 || ys.Count == 0) return;
+
+        var xMin = xs.Min(r => r.Min);
+        var xMax = xs.Max(r => r.Max);
+        var yMin = ys.Min(r => r.Min);
+        var yMax = ys.Max(r => r.Max);
+
+        if (xMin == xMax) { xMin -= 0.5; xMax += 0.5; }
+        if (yMin == yMax) { yMin -= 0.5; yMax += 0.5; }
+
+        XAxis.DataRange = new FRange(xMin, xMax);
+        YAxis.DataRange = new FRange(yMin, yMax);
+        Viewport.SetVisible(XAxis.DataRange, YAxis.DataRange);
+    }
+}
---
+++ FastCharts.Core/Series/LineSeries.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using FastCharts.Core.Abstractions;
+using FastCharts.Core.Primitives;
+
+namespace FastCharts.Core.Series;
+
+public sealed class LineSeries : ISeries<PointD>
+{
+    private readonly IReadOnlyList<PointD> _data;
+
+    public LineSeries(IEnumerable<PointD> points)
+    {
+        _data = points?.ToArray() ?? Array.Empty<PointD>();
+    }
+
+    public IReadOnlyList<PointD> Data => _data;
+
+    // UI-agnostic style hints
+    public double StrokeThickness { get; set; } = 1.0;
+    public double? StrokeOpacity { get; set; } = 1.0;
+
+    public bool IsEmpty => _data.Count == 0;
+
+    public FRange GetXRange()
+    {
+        if (_data.Count == 0) return new FRange(0, 0);
+        var min = _data.Min(p => p.X);
+        var max = _data.Max(p => p.X);
+        return new FRange(min, max);
+    }
+
+    public FRange GetYRange()
+    {
+        if (_data.Count == 0) return new FRange(0, 0);
+        var min = _data.Min(p => p.Y);
+        var max = _data.Max(p => p.Y);
+        return new FRange(min, max);
+    }
+}
