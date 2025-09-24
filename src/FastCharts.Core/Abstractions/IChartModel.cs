8873864c37345964feac1c82174d9eedc427ac10
Florent MENOT <florent.menot@setec.com>
2025-09-24 16:23:57 +0200

    Fix scatter series

--- src/FastCharts.Core/Abstractions/IChartModel.cs
+++ src/FastCharts.Core/Abstractions/IChartModel.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
+using FastCharts.Core.Axes;
+using FastCharts.Core.Series;
 
 namespace FastCharts.Core.Abstractions;
 
 public interface IChartModel
 {
-    IReadOnlyList<object> Axes { get; }   // kept loose for now; we’ll tighten later
-    IReadOnlyList<object> Series { get; } // idem
+    IReadOnlyList<NumericAxis> Axes { get; }
+    IReadOnlyList<SeriesBase> Series { get; }
 }
